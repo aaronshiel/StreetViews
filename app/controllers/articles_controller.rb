a6ee1b7bcecff409afd723abf4c4e216e12708e8
aaronshiel <ashiel408@gmail.com>
2021-09-04 19:39:50 -0700

    CRUD added for the Article model/resource

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,27 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   private
     def article_params
       #says that the params hash must have an :article key and that it is allowed to have
